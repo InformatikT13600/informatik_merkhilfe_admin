182f7cea7ffc1e27ef8d1ad86b97d7547b39f23c
Sether701 <sether017@gmail.com>
2021-04-23 16:33:54 +0200

    created some themedata

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
+import 'package:informatik_merkhilfe_admin/shared/styles.dart';
 import 'package:informatik_merkhilfe_admin/views/home.dart';
 
 void main() => runApp(MyApp());
@@ -10,7 +12,23 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Informatik Merkhilfe Adminpanel',
+      theme: ThemeData(
 
+        brightness: Brightness.dark,
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: colorMainBackground,
+
+        appBarTheme: AppBarTheme(
+          brightness: Brightness.dark,
+          textTheme: TextTheme(
+            headline6: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: colorMainAppbar,
+          titleTextStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+          centerTitle: true,
+        )
+
+      ),
       home: Home(),
     );
   }
