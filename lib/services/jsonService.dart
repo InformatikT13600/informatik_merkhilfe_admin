1910b8531867d3ca9c0d291c6448d08c498ab241
Sether701 <sether017@gmail.com>
2021-05-02 11:05:46 +0200

    languages can now be switched in order

--- lib/services/jsonService.dart
+++ lib/services/jsonService.dart
@@ -1,21 +1,69 @@
 import 'dart:convert';
 
+import 'package:informatik_merkhilfe_admin/models/language.dart';
+
 class JsonService {
 
   /// checks if a given [jsonString] is indeed a json
   static bool isJson(String jsonString) {
     try {
       jsonDecode(jsonString);
-      print('valid json');
       return true;
-    } on FormatException catch(e) {}
-    print('invalid json');
+    } on FormatException catch (e) {}
     return false;
+  }
+
+  /// takes a list of [Language]s and tries to encode it as a jsonString
+  static String writeLanguages(List<Language> list) {
+    return JsonEncoder.withIndent('  ').convert(list);
+  }
+
+
+  /// takes a [jsonString] and tries to decode it as a [List<Language>]
+  static List<Language> readLanguages(String jsonString) {
+    if(!validateLanguageJsonString(jsonString)) return [];
+
+    List<Language> list = [];
+
+    // read json array from json file
+    List<dynamic> languages = jsonDecode(jsonString);
+
+    // iterate through all language json objects
+    for(Map<String, dynamic> map in languages) {
+
+      // generate language object from json object
+      Language lang = Language.fromJSON(map);
+
+      list.add(lang);
+    }
+
+    return list;
   }
 
   /// checks if a given [jsonString] is a valid language json
   static bool validateLanguageJsonString(String jsonString) {
-    return false;
+    if(!isJson(jsonString)) return false;
+
+    try {
+      // check if json is a json array
+      if(!(jsonDecode(jsonString) is List<dynamic>)) return false;
+
+      // read json array from json file
+      List<dynamic> languages = jsonDecode(jsonString);
+
+      // iterate through all language json objects
+      for(Map<String, dynamic> map in languages) {
+
+        // generate language object from json object
+        Language lang = Language.fromJSON(map);
+
+        if(!lang.isValid()) return false;
+
+      }
+      return true;
+    } on FormatException catch(e) {
+      return false;
+    }
   }
 
 }
--- lib/views/languageEditor.dart
+++ lib/views/languageEditor.dart
@@ -1,13 +1,73 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:informatik_merkhilfe_admin/models/language.dart';
+import 'package:informatik_merkhilfe_admin/services/jsonService.dart';
+import 'package:informatik_merkhilfe_admin/views/section.dart';
 
 class LanguageEditor extends StatefulWidget {
+
+  final String controllerKey;
+
+  LanguageEditor(this.controllerKey);
+
   @override
   _LanguageEditorState createState() => _LanguageEditorState();
 }
 
 class _LanguageEditorState extends State<LanguageEditor> {
+  
+  List<Language> languages = [];
+  
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Language Editor'),);
+    languages = JsonService.readLanguages(Section.controllers[widget.controllerKey].value.text);
+
+    return ReorderableListView.builder(
+      key: UniqueKey(),
+      shrinkWrap: true,
+      header: TextButton(
+          onPressed: () {
+            Section.controllers[widget.controllerKey].text = JsonService.writeLanguages(languages);
+          },
+          child: Text('JSON updaten')
+      ),
+      itemCount: languages.length,
+      clipBehavior: Clip.hardEdge,
+      scrollDirection: Axis.vertical,
+      onReorder: (oldIndex, newIndex) {
+        // get moved language object
+        Language moved = languages[oldIndex];
+        // remove it from the list
+        languages.remove(moved);
+        // insert it at the new position
+        languages.insert(newIndex, moved);
+      },
+      itemBuilder: (context, index) {
+
+        Language lang = languages[index];
+
+        return Container(
+          key: Key(lang.name),
+          padding: EdgeInsets.all(5),
+          margin: EdgeInsets.only(bottom: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            border: Border.all(
+              color: lang.color,
+              width: 3,
+            )
+          ),
+          child: Row(
+            children: [
+              Text(
+                lang.name,
+                style: TextStyle(color: lang.color, fontSize: 30),
+              ),
+            ],
+          )
+        );
+      },
+    );
   }
 }
