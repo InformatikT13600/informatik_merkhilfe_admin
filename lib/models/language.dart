025a1c0dbfdb760d023f4515abb7aa888d5a4ae3
Sether701 <sether017@gmail.com>
2021-05-02 11:04:46 +0200

    created toJson Method for the language object

--- lib/models/language.dart
+++ lib/models/language.dart
@@ -3,15 +3,21 @@ import 'package:flutter/cupertino.dart';
 class Language {
 
   String name;
-  int orderPriority;
   String colorCode;
 
-  Language(this.name, this.orderPriority, this.colorCode);
+  Language(this.name, this.colorCode);
 
   Language.fromJSON(Map<String, dynamic> json) {
-    name = json['name'];
-    orderPriority = json['orderPriority'];
-    colorCode = json['colorCode'];
+    name = json['name'] == null ? '' : json['name'];
+    colorCode = json['colorCode'] == null ? '' : json['colorCode'];
+
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'colorCode': colorCode,
+    };
   }
 
   get color => Color(int.parse(this.colorCode, radix: 16));
